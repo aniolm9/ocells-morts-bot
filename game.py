f52cb75ffe5428da30af0c8dedd39ebdb77c6bf2
Aniol Martí <aniol@aniolmarti.cat>
2019-09-17 19:48:56 +0200

    Improve the game ending

--- game.py
+++ game.py
@@ -2,36 +2,45 @@
 import tools.numberGenerator as ng
 import tools.listSelector as ls
 import tools.fileProcessor as fp
+import tools.textToImage as tti
 import constants
 import sys
 
-def win():
-    return len(fp.readFile(constants.PEOPLE_LIST)) - len(fp.readFile(constants.DEAD_LIST)) == 1
+def win(people):
+    dead = fp.readFile(constants.DEAD_LIST)
+    if len(people) - len(dead) == 1:
+        winner = ls.selectPerson(people, dead)
+        print ("\n" + winner + " és l'últim supervivent. " + winner + " ha guanyat la partida.")
 
 def kill():
-    killer = ls.selectFromList(constants.PEOPLE_LIST)
-    victim = ls.selectFromList(constants.PEOPLE_LIST)
+    people = fp.readFile(constants.PEOPLE_LIST)
+    dead = fp.readFile(constants.DEAD_LIST)
+    killer = ls.selectPerson(people, dead)
+    victim = ls.selectPerson(people, dead)
     beerSentence = victim + " deu una cervesa a " + killer + "."
 
-    if win():
-        print (killer + " ha guanyat la partida.")
-        return constants.FI
+    if len(people) - len(dead) > 1:
+        if killer == victim:
+            print (victim + " ha estat víctima d'un apunyalament a Barcelona.")
+        elif (ng.getProbabilityNumber() == constants.LYNCHING) and (len(dead) < len(people)-5):
+            print (victim + " ha estat linxat públicament per fer un tuit massa dolent. " + victim + " deu una cervesa a tothom.")
+        # Opció troll 1:
+        elif victim == "@Virgili7":
+            print (killer + " a pegat un tiro a @Virgili7 i a morit. " + beerSentence)
+        # Opció troll 2:
+        elif victim == "@Joan_BonaNit":
+            print (killer + " ha introduit un megàfon a l'esòfag de @Joan_BonaNit. " + beerSentence)
+        else:
+            sentence = ls.selectSentence(fp.readFile(constants.SENTENCES_LIST))
+            print (killer + " " + sentence + " " + victim + ". " + beerSentence)
 
-    if killer == victim:
-        print (victim + " considera que el joc és una puta merda i s'ha suïcidat.")
-    elif ng.getProbabilityNumber() == constants.LYNCHING:
-        print (victim + " ha estat linxat públicament per fer un tuit massa dolent." + victim + " deu una cervesa a tothom.")
-    # Opció troll 1:
-    elif victim == "@Virgili7":
-        print (killer + " a pegat un tiro a @Virgili7 i a morit. " + beerSentence)
-    # Opció troll 2:
-    elif victim == "@Joan_BonaNit":
-        print (killer + " ha introduit un megàfon a l'esòfag de @Joan_BonaNit. " + beerSentence)
+        # Escriu la víctima a la llista de morts.
+        fp.appendToFile(constants.DEAD_LIST, victim + "\n")
+        tti.drawImage(constants.IMAGE)
+
+        # Check if somebody wins.
+        win(people)
     else:
-        sentence = ls.selectFromList(constants.SENTENCES_LIST)
-        print (killer + " " + sentence + " " + victim + ". " + beerSentence)
-
-    # Escriu la víctima a la llista de morts.
-    fp.appendToFile(constants.DEAD_LIST, victim + "\n")
+        sys.exit("El joc s'ha acabat.")
 
 kill()
