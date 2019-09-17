4190aa0d7597ca3e8cfb112b067d4c8498fab9c3
Aniol Martí <aniol@aniolmarti.cat>
2019-09-17 20:05:49 +0200

    Prepare environment for secret settings

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+import sys
+
+try:
+    import secret_settings.py
+except:
+    sys.exit("secret_settings.py not found.")
 
 PEOPLE_LIST = "resources/people.txt"
 DEAD_LIST = "resources/dead.txt"
