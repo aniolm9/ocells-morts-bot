318b9859f35bec9c79a0e1770f1040d0e960ee7c
Aniol Martí <aniol@aniolmarti.cat>
2019-09-17 17:36:04 +0200

    Add tool to generate random numbers

---
+++ tools/numberGenerator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import random
+import constants
+import tools.fileProcessor as fp
+
+# Number used to decide if it's a normal kill or a lynching.
+def getProbabilityNumber():
+    return random.randint(1, 155*5)
+
+# Number used to select the victim and the killer and the sentence.
+def getListNumber(l):
+    n = len(fp.readFile(l))
+    return random.randint(0, n-1)
