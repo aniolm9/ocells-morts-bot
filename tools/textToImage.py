fa5f7691206c88f03a0989dfc0780c734c7cbc8e
Aniol Martí <aniol@aniolmarti.cat>
2019-09-17 19:46:23 +0200

    Add tool to create an image
    Better way to display who is still alive in the game.

---
+++ tools/textToImage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from PIL import Image, ImageDraw, ImageFont
+import tools.fileProcessor as fp
+import constants
+
+def drawImage(imgFile):
+    people = fp.readFile(constants.PEOPLE_LIST)
+    dead = fp.readFile(constants.DEAD_LIST)
+
+    img = Image.new('RGB', (1080, 720), color=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font="/usr/share/fonts/truetype/freefont/FreeSerif.ttf", size=25)
+    color = (0, 0, 0)
+    x = 80
+    y = 70
+
+    for person in people:
+        if y >= 670:
+            x += 350
+            y = 70
+        if person in dead:
+            color = (255, 0, 0)
+        draw.text((x, y), person, fill=color, font=font)
+        color = (0, 0, 0)
+        y += 32
+    img.save(imgFile, "png")
