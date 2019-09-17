b201e348e38cc4ed2af05cd4ba737a7fd66e7922
Aniol Martí <aniol@aniolmarti.cat>
2019-09-17 17:37:02 +0200

    Add new function appendToFile

--- tools/fileProcessor.py
+++ tools/fileProcessor.py
@@ -2,8 +2,13 @@
 
 # Reads a file and returns a list with the lines.
 def readFile(file):
-    f = open(file, 'r+')
+    f = open(file, 'r')
     lines = f.readlines()
     content = [x.strip() for x in lines]
     f.close()
     return content
+
+def appendToFile(file, text):
+    f = open(file, 'a+')
+    f.write(text)
+    f.close()
