3a09e7f3b8896ccd23403e41584204f09067537b
Aniol Martí <aniol@aniolmarti.cat>
2019-09-18 12:03:56 +0200

    Implement main.py with a scheduler

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
+import settings
 import game
+import bot
+import schedule
+import time
 
-def main(DEBUG):
-    if DEBUG:
-        print(game.kill())
+def runnable(api):
+    text = game.kill()
+    print (text)
+    bot.newTweet(api, text, settings.IMAGE)
+
+def main():
+    # Just run the first time.
+    api = bot.createApi()
+
+    # Run every day at 18:00.
+    schedule.every().day.at("18:00").do(runnable, api=api)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == '__main__':
-    main(True)
+    main()
